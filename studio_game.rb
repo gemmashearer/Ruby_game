36df34b318970aec0bd796c66cc255e0eb9f51ee
Gemma Shearer <gemmaandjude@gmail.com>
2015-02-02 22:01:55 +0000

    added more variables

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,23 @@
 name1 = 'larry'
-health1 = 60
-
-puts "#{name1}'s health is #{health1 / 9}"
-
-puts "Players: \n \t Larry \n \t Curly \n \t Moe"
-
 name2 = 'Curly'
 name3 = 'Moe'
+name4 = 'shemp'
 
-puts "Players: \n\t#{name1} \n\t#{name2} \n\t#{name3}"
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
+
+puts "#{name1.capitalize} has a health of #{health1}"
+
+puts "#{name2.upcase} has a health of #{health2}"
+
+puts "#{name3.center(30, '*')} has a health of #{health3}"
+
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
+
