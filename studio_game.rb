3759dace361a171f819b68f862cc2d4a8168709e
Gemma Shearer <gemmaandjude@gmail.com>
2015-02-02 13:41:39 +0000

    First commit of ruby game

---
+++ studio_game.rb
@@ -0,0 +1,11 @@
+name1 = 'larry'
+health1 = 60
+
+puts "#{name1}'s health is #{health1 / 9}"
+
+puts "Players: \n \t Larry \n \t Curly \n \t Moe"
+
+name2 = 'Curly'
+name3 = 'Moe'
+
+puts "Players: \n\t#{name1} \n\t#{name2} \n\t#{name3}"
