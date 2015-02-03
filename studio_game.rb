43c75a48487eb7dc5eda1f819fce2a2ef7bac4f1
Gemma Shearer <gemmaandjude@gmail.com>
2015-02-03 22:47:57 +0000

    created new methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,23 +1,27 @@
-name1 = 'larry'
-name2 = 'Curly'
-name3 = 'Moe'
-name4 = 'shemp'
+def time
+  current_time = Time.new
+  current_time.strftime("%A %I:%M:%S")
+end
+  
 
-health1 = 60
-health2 = 125
-health3 = 100
-health4 = 90
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "The game started on #{formatted_time}"
 
-puts "#{name1.capitalize} has a health of #{health1}"
 
-puts "#{name2.upcase} has a health of #{health2}"
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
 
-puts "#{name3.center(30, '*')} has a health of #{health3}"
 
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
+
+
+
+
+
+
 
 
